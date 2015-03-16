5f7aa78dc151462501e5f4c8bc9484163231bfd9
Shown Tien <hightian@gmail.com>
2015-03-16 22:27:03 +0800

    proj init

---
+++ lib/traffic/monitor.rb
@@ -0,0 +1,7 @@
+require "traffic/monitor/version"
+
+module Traffic
+  module Monitor
+    autoload :Iftop, 'traffic/monitor/iftop'
+  end
+end
---
+++ lib/traffic/monitor/version.rb
@@ -0,0 +1,5 @@
+module Traffic
+  module Monitor
+    VERSION = "0.0.1"
+  end
+end
